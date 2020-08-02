280a35d2a16da7d5a769cb03bcc8660c40154c8b
rodnikovdi <rodnikovdi@vtblife.ru>
2020-08-03 01:17:14 +0300

    third attempt

---
+++ parallel-request-with-limit/parallel-requests-attempt-3.js
@@ -0,0 +1,31 @@
+function parallelRequests(urls, limit) {
+    return new Promise(resolve => {
+        const urlsDict = urls.reduce((acc, item, index) => {
+            acc[item] = acc[item] ? acc[item].concat(index) : [index];
+            return acc;
+        }, {});
+        const filteredUrls = Object.keys(urlsDict);
+        const result = [];
+        let lastFetchedIndex = 0;
+        
+        function makeRequest(from, amount) {
+            if (from >= filteredUrls.length || amount === 0) {
+                resolve(result);
+            }
+            
+            for (let i = from; i < from + amount; i++) {
+                const url = filteredUrls[i];
+                lastFetchedIndex = from + amount;
+                
+                fetch(url).then(response => {
+                    urlsDict[url].forEach(item => {
+                        result[item] = response;
+                    });
+                    makeRequest(lastFetchedIndex, 1);
+                });
+            }
+        }
+        
+        makeRequest(0, limit > urls.length ? urls.length : limit);
+    });
+}
